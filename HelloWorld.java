3084bbae82b0aa2f49e3180c5a7fa7a8f7a2cfaa
pranay kadu <kpranay@gmail.com>
2018-04-09 13:56:45 +0530

    added new files

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, World!");
+
+for(int i=0;i<11;i++){
+ System.out.println("Hello, World! " + i);
+}
+  }
+}
